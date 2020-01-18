c42606a6a4dfd0c4bc169bbe1c6b692bc887e8e7
Antonin Hildebrand <antonin@hildebrand.cz>
2020-01-18 18:41:32 +0100

    update box demo

--- examples/src/app/react_three_fiber/examples/demos/box.cljs
+++ examples/src/app/react_three_fiber/examples/demos/box.cljs
@@ -4,21 +4,24 @@
             [uix.core.alpha :as uix]
             [applied-science.js-interop :as j]))
 
-(defn rotate [node]
+(defn rotate! [node]
   (j/update-in! node [.-rotation .-x] #(+ % 0.01))
   (j/update-in! node [.-rotation .-y] #(+ % 0.01)))
 
+(defn update! [mesh]
+  (rotate! mesh))
+
 (defn <box> []
   (let [mesh-ref (uix/ref)]
-    (use-frame #(rotate @mesh-ref))
+    (use-frame #(update! @mesh-ref))
     [:mesh {:ref             mesh-ref
             :on-click        #(js/console.log "click")
             :on-pointer-over #(js/console.log "hover")
             :on-pointer-out  #(js/console.log "unhover")}
-     [:boxBufferGeometry {:attach "geometry" :args #js [1, 2, 3]}]
+     [:boxBufferGeometry {:attach "geometry" :args #js [1, 1, 1]}]
      [:meshNormalMaterial {:attach "material"}]]))
 
 (defn <app> []
-  [<:canvas> {:camera #js {:fov 75 :position #js [0 0 5]}}
+  [<:canvas> {:camera #js {:fov 75 :position #js [0 0 2]}}
    [<:suspense> {:fallback nil}
     [<box>]]])
