4bda0665ab2e616a7d3f060e09c252bf780008d8
Antonin Hildebrand <antonin@hildebrand.cz>
2020-01-19 01:44:18 +0100

    refactor intro page even more

--- examples/src/app/react_three_fiber/examples/pages/intro.cljs
+++ examples/src/app/react_three_fiber/examples/pages/intro.cljs
@@ -8,7 +8,10 @@
             [react-three-fiber.examples.lib.react-router-dom :refer [use-route-match <:switch> <:route> <:redirect> <:link>]]
             [react-three-fiber.examples.lib.helpers :refer [get-match-param]]))
 
-(def github-home-url "https://github.com/binaryage/cljs-react-three-fiber")
+; -- constants --------------------------------------------------------------------------------------------------------------
+
+(def home-url "https://github.com/binaryage/cljs-react-three-fiber")
+(def home-label "Github")
 
 ; -- data -------------------------------------------------------------------------------------------------------------------
 
@@ -21,10 +24,10 @@
 (defn lookup-component [name & [default]]
   (get-in all-demos [name :component] default))
 
-(defn prepare-allowed-paths [names]
+(defn all-allowed-paths [names]
   (map #(str "/demo/(" % ")") names))
 
-; -- style components -------------------------------------------------------------------------------------------------------
+; -- base components with styles --------------------------------------------------------------------------------------------
 
 (def <:page>
   (<:page-styles>
@@ -38,7 +41,7 @@
                  :right     "60px"
                  :font-size "1.2em"}})))
 
-(def <:demos-selection-panel>
+(def <:demo-selection-panel-base>
   (<:styled-div>
     (simple-css
       {:position  "absolute"
@@ -46,7 +49,7 @@
        :left      "50px"
        :max-width "260px"})))
 
-(def <:demo-selection-spot>
+(def <:demo-selection-button>
   (<:styled-div>
     (simple-css
       {:display       "inline-block"
@@ -62,43 +65,39 @@
 ; -- components -------------------------------------------------------------------------------------------------------------
 
 (defn <demo-canvas> [props <demo>]
+  (assert <demo>)
   (let [{:keys [name]} props]
     [:div#demo-canvas {:className (str "demo-" (string/lower-case name))}
      [<demo> {:name name}]]))
 
-(defn <demos-selection> []
+(defn <demo-selection-panel> []
   (let [match (use-route-match "/demo/(.*)")
-        selected-name (or (get-match-param match) default-demo-name)
-        bright? (get-in all-demos [selected-name :bright?])]
-    [<:demos-selection-panel>
-     (for [name all-demo-names]
-       (let [selected? (= name selected-name)
-             background-color (cond
-                                selected? "salmon"
-                                bright? "#2c2d31"
-                                :default "white")]
-         [<:link> {:key name
-                   :to  (str "/demo/" name)}
-          [<:demo-selection-spot> {:style {:background-color background-color}}]]))]))
+        selected-demo-name (or (get-match-param match) default-demo-name)
+        selected-demo (get all-demos selected-demo-name)]
+    [<:demo-selection-panel-base>
+     (for [demo-name all-demo-names]
+       (let [selected? (= demo-name selected-demo-name)
+             bg-color (cond
+                        selected? "salmon"
+                        (:bright? selected-demo) "#2c2d31"
+                        :default "white")]
+         [<:link> {:key demo-name :to (str "/demo/" demo-name)}
+          [<:demo-selection-button> {:style {:background-color bg-color}}]]))]))
 
 (defn <intro> []
   (let [match (use-route-match "/demo/(.*)")
-        selected-name (or (get-match-param match) default-demo-name)
-        bright? (get-in all-demos [selected-name :bright?])]
+        selected-demo-name (or (get-match-param match) default-demo-name)
+        selected-demo (get all-demos selected-demo-name)]
     [<:page>
      [:# {:fallback "loading intro page"}
       [<:switch>
        [<:route> {:exact    true
-                  :path     (->js (prepare-allowed-paths all-demo-names))
+                  :path     (->js (all-allowed-paths all-demo-names))
                   :children (fn [route-props]
                               (let [{:keys [match]} (bean route-props)
                                     selected-name (get-match-param match)
-                                    _ (assert selected-name)
-                                    component (lookup-component selected-name default-demo-name)
-                                    _ (assert component)]
+                                    component (lookup-component selected-name default-demo-name)]
                                 (as-element (<demo-canvas> {:name selected-name} component))))}]
        [<:redirect> {:to (str "/demo/" default-demo-name)}]]]
-     [<demos-selection>]
-     [:a {:href  github-home-url
-          :style {:color (if bright? "#2c2d31" "white")}}
-      "Github"]]))
+     [<demo-selection-panel>]
+     [:a {:href home-url :style {:color (if (:bright? selected-demo) "#2c2d31" "white")}} home-label]]))
