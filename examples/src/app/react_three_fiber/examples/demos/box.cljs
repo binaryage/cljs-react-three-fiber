9bca2546bf6d2ea360bb56683d87db4ab7ca2d10
Antonin Hildebrand <antonin@hildebrand.cz>
2020-01-23 15:22:32 +0100

    refine box demo

--- examples/src/app/react_three_fiber/examples/demos/box.cljs
+++ examples/src/app/react_three_fiber/examples/demos/box.cljs
@@ -1,14 +1,25 @@
 (ns react-three-fiber.examples.demos.box
   (:require [uix.core.alpha :as uix]
-            [react-three-fiber.core :refer [<:canvas> use-three use-frame]]
+            [react-three-fiber.core :refer [<:canvas> use-frame]]
             [react-three-fiber.examples.lib.helpers :refer [update-rotation!]]))
 
-(defn rotate! [node]
+; -- constants --------------------------------------------------------------------------------------------------------------
+
+(def camera-config #js {:fov      75
+                        :position #js [0 0 2]})
+
+; -- helpers ----------------------------------------------------------------------------------------------------------------
+
+(defn rotate! [node dx dy dz]
   (update-rotation! node (fn [x y z]
-                           [(+ x 0.01) (+ y 0.01) z])))
+                           [(+ x dx) (+ y dy) (+ z dz)])))
+
+; -- update loop ------------------------------------------------------------------------------------------------------------
 
 (defn update! [mesh]
-  (rotate! mesh))
+  (rotate! mesh 0.01 0.01 0))
+
+; -- components -------------------------------------------------------------------------------------------------------------
 
 (defn <box> []
   (let [mesh-ref (uix/ref)]
@@ -17,9 +28,10 @@
             :on-click        #(js/console.log "click")
             :on-pointer-over #(js/console.log "hover")
             :on-pointer-out  #(js/console.log "unhover")}
-     [:boxBufferGeometry {:attach "geometry" :args #js [1, 1, 1]}]
+     [:boxBufferGeometry {:attach "geometry"
+                          :args   #js [1, 1, 1]}]
      [:meshNormalMaterial {:attach "material"}]]))
 
 (defn <demo> []
-  [<:canvas> {:camera #js {:fov 75 :position #js [0 0 2]}}
+  [<:canvas> {:camera camera-config}
    [<box>]])
