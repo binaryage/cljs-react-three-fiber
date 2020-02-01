ec9350c4d1a19e9f6018a5dd9daaf1d829903d3c
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-01 17:20:52 +0100

    by default generate reports with a timestamp under .reports dir

--- examples/scripts/report.sh
+++ examples/scripts/report.sh
@@ -5,7 +5,8 @@ set -e -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 PROJECT_ROOT_DIR=$(pwd -P)
-REPORT_PATH="$PROJECT_ROOT_DIR/.report.html"
+REPORT_NAME=${1:-"report-$(date +"%Y-%m-%d-%H-%M-%S")"}
+REPORT_PATH="$PROJECT_ROOT_DIR/.reports/$REPORT_NAME.html"
 
 set -x
 clj -A:shadow-cljs run shadow.cljs.build-report app "$REPORT_PATH"
