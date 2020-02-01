9e3dc64734020449a24896c9105dff525b7f92ff
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-01 20:44:08 +0100

    scripts: make sure report script writes into existing directory

--- examples/scripts/report.sh
+++ examples/scripts/report.sh
@@ -8,6 +8,8 @@ PROJECT_ROOT_DIR=$(pwd -P)
 REPORT_NAME=${1:-"report-$(date +"%Y-%m-%d-%H-%M-%S")"}
 REPORT_PATH="$PROJECT_ROOT_DIR/.reports/$REPORT_NAME.html"
 
+mkdir -p "$PROJECT_ROOT_DIR/.reports"
+
 set -x
 clj -A:shadow-cljs run shadow.cljs.build-report app "$REPORT_PATH"
 
