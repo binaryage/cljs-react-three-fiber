e4a7c902f2476ba8b463b7e998a96bbb63bc87e3
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-03 23:31:24 +0100

    hide interop in lib.dom

--- examples/src/app/react_three_fiber/examples/index.cljs
+++ examples/src/app/react_three_fiber/examples/index.cljs
@@ -1,9 +1,10 @@
 (ns react-three-fiber.examples.index
   (:require [shadow.loader :as shadow-loader]
-            [applied-science.js-interop :as j]
             [react-three-fiber.examples.lib.ui :as ui :refer [defnc $]]
             [react-three-fiber.examples.lib.react-router-dom :refer [<router>]]
-            [react-three-fiber.examples.lib.dom :refer [get-element-by-id]]
+            [react-three-fiber.examples.lib.dom :refer [get-element-by-id
+                                                        get-window-location-origin
+                                                        get-window-location-pathname]]
             [react-three-fiber.examples.pages.intro :refer [<intro>]]
             [react-three-fiber.core :refer [use-frame <canvas>]]
             [react-three-fiber.examples.styles :refer [<global-styles>]]
@@ -25,8 +26,8 @@
 
 (defn init-module-loader! []
   ; https://github.com/thheller/shadow-cljs/pull/646
-  (let [origin (j/get-in goog/global [:window :location :origin])
-        pathname (j/get-in goog/global [:window :location :pathname])
+  (let [origin (get-window-location-origin)
+        pathname (get-window-location-pathname)
         prefix (remove-trailing-slashes (str origin pathname))]
     (shadow-loader/init prefix)))
 
--- examples/src/app/react_three_fiber/examples/lib/dom.cljs
+++ examples/src/app/react_three_fiber/examples/lib/dom.cljs
@@ -2,3 +2,9 @@
 
 (defn get-element-by-id [id]
   (js/document.getElementById id))
+
+(defn get-window-location-origin []
+  js/window.location.origin)
+
+(defn get-window-location-pathname []
+  js/window.location.pathname)
